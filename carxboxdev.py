f8c031ebef8c1c90bcbf7926a93eb15715b2e4c8
JW Yeh <airwaveyjw@gmail.com>
2016-04-19 04:13:59 +0800

    :

--- carxboxdev.py
+++ carxboxdev.py
@@ -24,42 +24,28 @@ pygame.joystick.init()
 
 
 def padprintout():
-# Get count of joysticks
+    # Get count of joysticks
     J_count = pygame.joystick.get_count()
     for i in range(J_count):
         JX = pygame.joystick.Joystick(i)
         JX.init()
-        print("Joystick {}".format(i) )
         # Get the name from the OS for the controller/joystick
         name = JX.get_name()   #名字
-        print("Joystick name: {}".format(name) )
-    ##############################################################################
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
         axes = JX.get_numaxes()  #
-        print("Number of axes: {}".format(axes) )
         a = JX.get_axis
-        print("LP[{:>6.3f},{:>6.3f}],RP[{:>6.3f},{:>6.3f}],RT[{:>6.3f}],LT[{:>6.3f}]"
-        .format(a(0),a(1),a(2),a(3),a(4),a(5)))
-    ##############################################################################
-        #get buttons
         buttons = JX.get_numbuttons()
-        print("Number of buttons: {}".format(buttons) )
         b=JX.get_button
-        print("A:{},B:{},X:{},Y:{},LB:{},RB:{},BACK:{},START:{},XBOX:{},LP:{},RP:{}"
-        .format(b(0),b(1),b(2),b(3),b(4),b(5),b(6),b(7),b(8),b(9),b(10)) )
-    #############################################################################
-        # Hat switch. All or nothing for direction, not like joysticks.
-        # Value comes back in an array.
         hats = JX.get_numhats()
-        print("Number of hats: {}".format(hats) )
         hat = JX.get_hat(i)
-        print("Hat {} value: {}".format(i, str(hat)) )
-    #################################################################
+
+        print("Joystick {} : {} LP[{:>6.3f},{:>6.3f}],LT[{:>6.3f}] <=> RP[{:>6.3f},{:>6.3f}],RT[{:>6.3f}] ::  A:{},B:{},X:{},Y:{} = LB:{},RB:{} LP:{},RP:{} = BACK:{},START:{},XBOX:{}  :: Hat {} value: {}""
+        .format(i,name,a(0),a(1),a(2),a(3),a(4),a(5),b(0),b(1),b(2),b(3),b(4),b(5),b(9),b(10),b(6),b(7),b(8),i, str(hat)))
+
+
         if JX.get_button(8)==1: # If user clicked close
-            #done=True # Flag that we are done so we exit this loop
-            pygame.quit()
-            quit()
+        #done=True # Flag that we are done so we exit this loop
+        pygame.quit()
+        quit()
 
 # -------- Main Program Loop -----------
 padprintout()
@@ -68,13 +54,13 @@ while done==False:
 
     for event in pygame.event.get(): # User did something
         # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        padprintout()
+
         if event.type == pygame.JOYBUTTONDOWN:
             print("Joystick button pressed.")
         if event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
 
-
+    padprintout()
     # Limit to 20 frames per second
     clock.tick(1)
 
