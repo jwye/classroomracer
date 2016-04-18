2974a6acd97e06b24ecb405d241905b582eca456
JW Yeh <airwaveyjw@gmail.com>
2016-04-19 04:14:45 +0800

    :

--- carxboxdev.py
+++ carxboxdev.py
@@ -38,7 +38,7 @@ def padprintout():
         hats = JX.get_numhats()
         hat = JX.get_hat(i)
 
-        print("Joystick {} : {} LP[{:>6.3f},{:>6.3f}],LT[{:>6.3f}] <=> RP[{:>6.3f},{:>6.3f}],RT[{:>6.3f}] ::  A:{},B:{},X:{},Y:{} = LB:{},RB:{} LP:{},RP:{} = BACK:{},START:{},XBOX:{}  :: Hat {} value: {}""
+        print("Joystick {} : {} LP[{:>6.3f},{:>6.3f}],LT[{:>6.3f}] <=> RP[{:>6.3f},{:>6.3f}],RT[{:>6.3f}] ::  A:{},B:{},X:{},Y:{} = LB:{},RB:{} LP:{},RP:{} = BACK:{},START:{},XBOX:{}  :: Hat {} value: {}"
         .format(i,name,a(0),a(1),a(2),a(3),a(4),a(5),b(0),b(1),b(2),b(3),b(4),b(5),b(9),b(10),b(6),b(7),b(8),i, str(hat)))
 
 
