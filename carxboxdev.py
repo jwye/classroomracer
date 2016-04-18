48997e8d0071bcd0122c10eae8642e92cba5817c
JW Yeh <airwaveyjw@gmail.com>
2016-04-19 00:11:47 +0800

    d

--- carxboxdev.py
+++ carxboxdev.py
@@ -17,47 +17,47 @@ pygame.joystick.init()
 
 # -------- Main Program Loop -----------
 while done==False:
-    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-    # Get count of joysticks
-    JX_count = pygame.joystick.get_count()
+# ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+        # Get count of joysticks
+        JX_count = pygame.joystick.get_count()
     for i in range(joystick_count):
         JX = pygame.joystick.Joystick(i)
         JX.init()
         print("Joystick {}".format(i) )
-    # Get the name from the OS for the controller/joystick
-    name = JX.get_name()   #名字
-    print("Joystick name: {}".format(name) )
-##############################################################################
-    # Usually axis run in pairs, up/down for one, and left/right for
-    # the other.
-    axes = JX.get_numaxes()  #
-    print("Number of axes: {}".format(axes) )
-    for i in range( axes ):
-        axis = JX.get_axis( i )
-        print("Axis {} value: {:>6.3f}".format(i, axis) )
-##############################################################################
-    #get buttons
-    buttons = JX.get_numbuttons()
-    print("Number of buttons: {}".format(buttons) )
-    for i in range( buttons ):
-        button = JX.get_button( i )
-        print("Button {:>2} value: {}".format(i,button) )
-#############################################################################
-    # Hat switch. All or nothing for direction, not like joysticks.
-    # Value comes back in an array.
-    hats = JX.get_numhats()
-    print("Number of hats: {}".format(hats) )
-    for i in range( hats ):
-        hat = JX.get_hat( i )
-        print("Hat {} value: {}".format(i, str(hat)) )
-##################################################################
-    if JX.get_button(8)==1:
-        done=True
-    # Limit to 20 frames per second
-    clock.tick(20)
+        # Get the name from the OS for the controller/joystick
+        name = JX.get_name()   #名字
+        print("Joystick name: {}".format(name) )
+    ##############################################################################
+        # Usually axis run in pairs, up/down for one, and left/right for
+        # the other.
+        axes = JX.get_numaxes()  #
+        print("Number of axes: {}".format(axes) )
+        for i in range( axes ):
+            axis = JX.get_axis( i )
+            print("Axis {} value: {:>6.3f}".format(i, axis) )
+    ##############################################################################
+        #get buttons
+        buttons = JX.get_numbuttons()
+        print("Number of buttons: {}".format(buttons) )
+        for i in range( buttons ):
+            button = JX.get_button( i )
+            print("Button {:>2} value: {}".format(i,button) )
+    #############################################################################
+        # Hat switch. All or nothing for direction, not like joysticks.
+        # Value comes back in an array.
+        hats = JX.get_numhats()
+        print("Number of hats: {}".format(hats) )
+        for i in range( hats ):
+            hat = JX.get_hat( i )
+            print("Hat {} value: {}".format(i, str(hat)) )
+    ##################################################################
+        if JX.get_button(8)==1:
+            done=True
+        # Limit to 20 frames per second
+        clock.tick(10)
 
-    # Close the window and quit.
-    # If you forget this line, the program will 'hang'
-    # on exit if running from IDLE.
+        # Close the window and quit.
+        # If you forget this line, the program will 'hang'
+        # on exit if running from IDLE.
 pygame.quit()
 quit()
