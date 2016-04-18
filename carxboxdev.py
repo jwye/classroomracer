1a86468cd59cdcba410cf8c86d78518d5feac00c
JW Yeh <airwaveyjw@gmail.com>
2016-04-19 02:15:19 +0800

    :

--- carxboxdev.py
+++ carxboxdev.py
@@ -25,54 +25,52 @@ pygame.joystick.init()
 # -------- Main Program Loop -----------
 
 while done==False:
-    event = pygame.event.get()# User did something
+#    for event in pygame.event.get(): # User did something
         # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-            # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-                # Get count of joysticks
-                J_count = pygame.joystick.get_count()
+#        if event.type == pygame.JOYBUTTONDOWN:
+#            print("Joystick button pressed.")
+#        if event.type == pygame.JOYBUTTONUP:
+#            print("Joystick button released.")
+# ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+    # Get count of joysticks
+    J_count = pygame.joystick.get_count()
 
-                for i in range(J_count):
-                    JX = pygame.joystick.Joystick(i)
-                    JX.init()
-                    print("Joystick {}".format(i) )
-                    # Get the name from the OS for the controller/joystick
-                    name = JX.get_name()   #名字
-                    print("Joystick name: {}".format(name) )
-                ##############################################################################
-                    # Usually axis run in pairs, up/down for one, and left/right for
-                    # the other.
-                    axes = JX.get_numaxes()  #
-                    print("Number of axes: {}".format(axes) )
-                    for i in range( axes ):
-                        axis = JX.get_axis( i )
-                        print("Axis {} value: {:>6.3f}".format(i, axis) )
-                ##############################################################################
-                    #get buttons
-                    buttons = JX.get_numbuttons()
-                    print("Number of buttons: {}".format(buttons) )
-                    for i in range( buttons ):
-                        button = JX.get_button( i )
-                        print("Button {:>2} value: {}".format(i,button) )
-                #############################################################################
-                    # Hat switch. All or nothing for direction, not like joysticks.
-                    # Value comes back in an array.
-                    hats = JX.get_numhats()
-                    print("Number of hats: {}".format(hats) )
-                    for i in range( hats ):
-                        hat = JX.get_hat( i )
-                        print("Hat {} value: {}".format(i, str(hat)) )
-                #################################################################
-                    # Limit to 20 frames per second
-                    if JX.get_button(8)==1: # If user clicked close
-                        done=True # Flag that we are done so we exit this loop
+    for i in range(J_count):
+        JX = pygame.joystick.Joystick(i)
+        JX.init()
+        print("Joystick {}".format(i) )
+        # Get the name from the OS for the controller/joystick
+        name = JX.get_name()   #名字
+        print("Joystick name: {}".format(name) )
+    ##############################################################################
+        # Usually axis run in pairs, up/down for one, and left/right for
+        # the other.
+        axes = JX.get_numaxes()  #
+        print("Number of axes: {}".format(axes) )
+        for i in range( axes ):
+            axis = JX.get_axis( i )
+            print("Axis {} value: {:>6.3f}".format(i, axis) )
+    ##############################################################################
+        #get buttons
+        buttons = JX.get_numbuttons()
+        print("Number of buttons: {}".format(buttons) )
+        for i in range( buttons ):
+            button = JX.get_button( i )
+            print("Button {:>2} value: {}".format(i,button) )
+    #############################################################################
+        # Hat switch. All or nothing for direction, not like joysticks.
+        # Value comes back in an array.
+        hats = JX.get_numhats()
+        print("Number of hats: {}".format(hats) )
+        for i in range( hats ):
+            hat = JX.get_hat( i )
+            print("Hat {} value: {}".format(i, str(hat)) )
+    #################################################################
+        # Limit to 20 frames per second
+        if JX.get_button(8)==1: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
 
-                # Limit to 20 frames per second
-        if event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
-
-
+    # Limit to 20 frames per second
     clock.tick(20)
         # Close the window and quit.
         # If you forget this line, the program will 'hang'
