acd69930239860e871726a7ebd84338cb0069cb2
JW Yeh <airwaveyjw@gmail.com>
2016-04-19 04:15:55 +0800

    :

--- carxboxdev.py
+++ carxboxdev.py
@@ -42,9 +42,7 @@ def padprintout():
         .format(i,name,a(0),a(1),a(2),a(3),a(4),a(5),b(0),b(1),b(2),b(3),b(4),b(5),b(9),b(10),b(6),b(7),b(8),i, str(hat)))
 
 
-        if JX.get_button(8)==1: # If user clicked close
-        #done=True # Flag that we are done so we exit this loop
-        pygame.quit()
+        if JX.get_button(8)==1: # If user clicked close    #done=True # Flag that we are done so we exit this loop
         quit()
 
 # -------- Main Program Loop -----------
