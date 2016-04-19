62b220cd8e39ca2253c72f631523c3ce8b7d6501
JW Yeh <airwaveyjw@gmail.com>
2016-04-19 23:31:23 +0800

    :

--- carxboxdev.py
+++ carxboxdev.py
@@ -30,8 +30,12 @@ def padprintout():
         hats = JX.get_numhats()
         hat = JX.get_hat(i)
 
-        print("Joystick {} : {} = LP[{:>6.2f},{:>6.2f}],LT[{:>6.2f}] || RP[{:>6.2f},{:>6.2f}],RT[{:>6.2f}] || A:{},B:{},X:{},Y:{} = LB:{},RB:{} LP:{},RP:{} || BACK:{},START:{},XBOX:{} || hat: {}"
-        .format(i,name,a(0),-a(1),(a(2)+1)/2,a(3),-a(4),(a(5)+1)/2,b(0),b(1),b(2),b(3),b(4),b(5),b(9),b(10),b(6),b(7),b(8), str(hat)))
+        print("Joystick {} : {} = LP[{:>6.2f},{:>6.2f}],LT[{:>6.2f}] \
+        || RP[{:>6.2f},{:>6.2f}],RT[{:>6.2f}] \
+        || A:{},B:{},X:{},Y:{} = LB:{},RB:{} LP:{},RP:{} \
+        || BACK:{},START:{},XBOX:{} || hat: {}"
+        .format(i,name,a(0),-a(1),(a(2)+1)/2,a(3),-a(4),(a(5)+1)/2,\
+        b(0),b(1),b(2),b(3),b(4),b(5),b(9),b(10),b(6),b(7),b(8), str(hat)))
 
 
         if JX.get_button(8)==1: # If user clicked close    #done=True # Flag that we are done so we exit this loop
@@ -45,7 +49,7 @@ while done==False:
 
     for event in pygame.event.get(): # User did something
         # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP or
+        if event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP or \
         event.type == JOYAXISMOTION or event.type == JOYHATMOTION:
             padprintout()
             clock.tick(4)
