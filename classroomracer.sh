7f7175ed4a8cecde86ffcaf59d08860230449971
JW Yeh <airwaveyjw@gmail.com>
2016-04-22 00:24:53 +0800

    :

--- classroomracer.sh
+++ classroomracer.sh
@@ -2,6 +2,6 @@
 ## add at end   = sudo sh /home/pi/Development/classroomracer/classroomracer.sh
 cd /
 cd home/pi/
-sudo python3 ./Development/classroomracer/carxboxdev.py  &
+python3 ./Development/classroomracer/carxboxdev.py  &
 exit 0
 cd /
