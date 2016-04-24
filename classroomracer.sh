3c821182b9d12ba742de94648765885e09cafa3c
JW Yeh <airwaveyjw@gmail.com>
2016-04-25 04:11:15 +0800

    :

--- classroomracer.sh
+++ classroomracer.sh
@@ -4,7 +4,7 @@
 # add bluetooth= echo "connect 14:10:9F:E0:7B:DE \nquit" | bluetoothctl
 ## add at end   = sh /home/pi/Development/classroomracer/classroomracer.sh
 
-echo "connect 14:10:9F:E0:7B:DE \nquit" | bluetoothctl
+#echo "connect 14:10:9F:E0:7B:DE \nquit" | bluetoothctl
 
 cd /
 cd home/pi/
