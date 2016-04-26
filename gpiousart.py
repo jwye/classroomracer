7cd1f8c60d9ecb00cf8a2e2258bf0ae2cc1bf313
JW Yeh <airwaveyjw@gmail.com>
2016-04-26 20:10:35 +0800

    :

---
+++ gpiousart.py
@@ -0,0 +1,28 @@
+#using gpio to simulate st32 9 bits usart
+#hoverboart protocal
+
+import time
+import RPi.GPIO as GPIO
+
+Baudrate = 26640
+#ONE Bit is 38us
+Bittime=40/1000000
+BitsinByte = 9
+#MDBmode:address indicated MSB=1
+MDB=1
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+try:
+    while 1:
+        GPIO.output(11, GPIO.HIGH)
+        time.sleep(Bittime)
+        GPIO.output(11, GPIO.LOW)
+        time.sleep(Bittime)
+        GPIO.output(11, GPIO.HIGH)
+        time.sleep(Bittime)
+        GPIO.output(11, GPIO.LOW)
+        time.sleep(Bittime)
+except(KeyboardInterrupt):
+    GPIO.cleanup()
--- uarttest.py
+++ uarttest.py
@@ -9,53 +9,36 @@ import serial
 import time
 import binascii
 
+
 ser = serial.Serial()
-ser.port= '/dev/tty.usbserial'
+#ser.port= '/dev/tty.usbserial'
+ser.port= '/dev/cu.usbserial'
 ser.baudrate = 26640
 ser.timeout= 1
-ser.parity='O'
+ser.parity=serial.PARITY_NONE
 
 ser.open()
 
 print(ser.isOpen())
 try:
-    for i in range(50000):
-        #arr1 = bytearray([0xAA,0x00,0x12,0x03,0x12,0x03])
+    for i in range(2000):
+        arr1 = bytearray([0x55,0x00,0x13,0x02,0x13,0x02])
         #arr1 = bytearray([0xAA,0xAA,0xFC,0x03,0xFC,0x03])
         #arr1 = bytearray([0xAA,0x80,0xFC,0x03,0xFC,0x03])
-        arr1=bytearray([0xAA])
-        #ser.parity="PARITY_NONE"
-        #ser.parity='N'
         ser.write(arr1)
 
-        #ser.parity='E'
-        #ser.write(arr1)
 
-        #ser.parity='O'
-        #ser.write(arr1)
-
-        #ser.close()
-        #ser.parity='S'
-        #ser.open()
-        #ser.write(arr1)
-
-        #ser.close()
-        #ser.parity='M'
-        #ser.open()
-        #ser.write(arr1)
-        #arr2=0x00
-        #ser.write(arr2)
-
-        #arr3=0x55
-        #ser.write(arr3)
 except(KeyboardInterrupt, SystemExit):
     ser.close()
 
-while 1:
+print(ser.isOpen())
+
+while True:
     try:
 
         #arr1 = bytearray([0xAA,0x00,0x10,0x10])
-        arr1 = bytearray([0x55,0x80,0xFC,0x03,0xFC,0x03])
+        arr1 = bytearray([0x55,0x00,0x13,0x02,0x13,0x02])
+        #arr1 = bytearray([0b101010100,0b000000001])
         ser.write(arr1)
 
 
