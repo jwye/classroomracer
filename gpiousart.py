f2d4b8970a7f7364f6c4f6159784935aa3cfc018
JW Yeh <airwaveyjw@gmail.com>
2016-04-26 21:49:09 +0800

    :

--- gpiousart.py
+++ gpiousart.py
@@ -6,21 +6,74 @@ import RPi.GPIO as GPIO
 
 Baudrate = 26640
 #ONE Bit is 38us
-Bittime=40/1000000
+Bittime=10/10000000
 BitsinByte = 9
 #MDBmode:address indicated MSB=1
 MDB=1
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-
+GPIO.output(11, GPIO.HIGH)
+print('GPIO 11 is set to HIGH')
+time.sleep(5)
 try:
+    print('GO')
     while 1:
+            time.sleep(30*Bittime)
 
-        GPIO.output(11, GPIO.HIGH)
-        #time.sleep(Bittime)
-        GPIO.output(11, GPIO.LOW)
-        #time.sleep(Bittime)
-        
+            # 0xAA 0xAA  0b 1 10101010
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(Bittime) # start
+            # 1010
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(Bittime) # lsb
+
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(Bittime) # 2
+
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(Bittime) # 3
+
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(Bittime) # 4
+            # 1010
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(Bittime) # lsb
+
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(Bittime)
+
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(Bittime) # lsb
+
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(Bittime)
+
+            # MSB=0
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(Bittime)
+            # finish 0xAA  0b 1 10101010
+
+            #pandding
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(6*Bittime)
+            #
+
+
+            # 0b00000000
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(8*Bittime)
+            # MSB=1
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(Bittime)
+            # first 9 bits
+
+
+
+
+
+
+
+
 except(KeyboardInterrupt):
     GPIO.cleanup()
