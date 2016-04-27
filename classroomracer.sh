4f1fa278e82bed19ef8d77d39ad52f8682018bf0
JW Yeh <airwaveyjw@gmail.com>
2016-04-28 04:03:46 +0800

    no message

--- classroomracer.sh
+++ classroomracer.sh
@@ -1,5 +1,6 @@
 ##sudo nano /etc/profile  ## autostart on boot
-## or add to /etc/rc.local
+
+## or add to /etc/rc.local  # ichoose this one
 
 # add bluetooth= echo "connect 14:10:9F:E0:7B:DE \nquit" | bluetoothctl
 ## add at end   = sh /home/pi/Development/classroomracer/classroomracer.sh
